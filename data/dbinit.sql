6d628e21cdff60e9fce5b12754716f0581936096
i90r.rodionov <info@igor-rodionov>
2022-02-18 01:36:19 +0700

    update pg-job

--- data/dbinit.sql
+++ data/dbinit.sql
@@ -16,11 +16,11 @@ create table t_user
 
 insert into T_USER
 values (1, --c_id             bigint not null,
-        'Username', -- c_username       varchar(256),
-        'Firstname', -- c_firstname      varchar(128),
-        'Lastname', -- c_lastname varchar(128),
-        'user@domain.root', -- c_email varchar(256),
-        '71231231212', --c_phone varchar(30),
+        'Username1', -- c_username       varchar(256),
+        'Firstname1', -- c_firstname      varchar(128),
+        'Lastname1', -- c_lastname varchar(128),
+        'user1@domain.root', -- c_email varchar(256),
+        '71111111111', --c_phone varchar(30),
         current_timestamp, -- c_createdate timestamp,
         current_timestamp, -- c_lastchangedate timestamp,
         0 --c_version bigint
@@ -32,7 +32,18 @@ values (2, --c_id             bigint not null,
         'Firstname2', -- c_firstname      varchar(128),
         'Lastname2', -- c_lastname varchar(128),
         'user2@domain.root', -- c_email varchar(256),
-        '79876543210', --c_phone varchar(30),
+        '72222222222', --c_phone varchar(30),
+        current_timestamp, -- c_createdate timestamp,
+        current_timestamp, -- c_lastchangedate timestamp,
+        0 --c_version bigint
+       );
+insert into T_USER
+values (3, --c_id             bigint not null,
+        'Username3', -- c_username       varchar(256),
+        'Firstname3', -- c_firstname      varchar(128),
+        'Lastname3', -- c_lastname varchar(128),
+        'user3@domain.root', -- c_email varchar(256),
+        '73333333333', --c_phone varchar(30),
         current_timestamp, -- c_createdate timestamp,
         current_timestamp, -- c_lastchangedate timestamp,
         0 --c_version bigint
